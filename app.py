3364ae7f8458aec694906dac799c2592fa02a6d8
Ammyvijay <ammyvijay.b@gmail.com>
2023-03-07 11:47:58 +0530

    Change Docker File Config

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ def home():
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
-    print(data)
+    # print(data)
     # print(np.array(list(data.values())).reshape(1,-1))
     new_data=scamodel.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
-    print(output[0])
+    # print(output[0])
     return jsonify(math.floor(output[0]))
 
 @app.route('/predict',methods=['POST'])
@@ -32,5 +32,5 @@ def predict():
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=int("3000"),debug=True)
+    app.run(debug=False)
 
